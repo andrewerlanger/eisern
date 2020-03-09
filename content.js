03d3da32d5d07f769a8a6c1c605de7ba99b76847
Andrew Erlanger <a.erlanger@gmail.com>
2020-03-09 20:30:59 +0100

    update area query

--- content.js
+++ content.js
@@ -1,48 +1,50 @@
 var linkHelper = {
-  init: function () {
+  init: function() {
     chrome.storage.sync.get("running", result => {
       if (result.running != false) {
         this.checkPage();
       }
     });
   },
-  checkPage: function () {
+  checkPage: function() {
     var regex = /.*union-zeughaus\.de\/union-berlin\/spticket/g;
     if (regex.test(window.location.href)) {
       this.searchMap();
     }
   },
-  searchMap: function () {
+  searchMap: function() {
     var map = document.querySelector("map[name='spmap']");
     var areas = this.getAreas(map);
     areas.length > 0 ? this.selectArea(areas) : this.refreshPage();
   },
-  getAreas: function (map) {
+  getAreas: function(map) {
     if (!map) {
       return [];
     }
 
-    return map.querySelectorAll("area[shape='polygon'], area[shape='rectangle']");
+    return map.querySelectorAll(
+      "area[shape='polygon'], area[shape='rectangle'], area[shape='rect']"
+    );
   },
-  selectArea: async function (areas) {
+  selectArea: async function(areas) {
     areas[0].click();
+    this.injectAudio();
     await this.sleep();
     this.selectTicket();
   },
-  selectTicket: function () {
+  selectTicket: function() {
     var ticketContainer = document.querySelector(".KartentypZeile");
     ticketContainer.querySelectorAll("div.BGNr").forEach(ticket => {
       ticket.innerText == "1" && ticket.click();
     });
     this.checkOut();
   },
-  checkOut: function () {
+  checkOut: function() {
     var checkoutLink = document.getElementById("ZurKasseUrl").value;
     window.open(checkoutLink, "_blank");
-    this.injectAudio();
     this.setRunningState(false);
   },
-  injectAudio: function () {
+  injectAudio: function() {
     var audio = document.createElement("audio");
     audio.id = "audioAlert";
     audio.preload = "auto";
@@ -51,19 +53,20 @@ var linkHelper = {
     document.body.appendChild(audio);
     audio.play();
   },
-  setRunningState: function (runningState) {
+  setRunningState: function(runningState) {
     chrome.storage.sync.set({
       running: runningState
-    })
+    });
   },
-  refreshPage: async function () {
+  refreshPage: async function() {
     await this.sleep();
     location.reload();
   },
-  sleep: function () {
+  sleep: function() {
     return new Promise(resolve => setTimeout(resolve, 2000));
   }
 };
 
-var audioSource = "https://notificationsounds.com/soundfiles/08b255a5d42b89b0585260b6f2360bdd/file-sounds-1137-eventually.mp3";
+var audioSource =
+  "https://notificationsounds.com/soundfiles/08b255a5d42b89b0585260b6f2360bdd/file-sounds-1137-eventually.mp3";
 linkHelper.init();
